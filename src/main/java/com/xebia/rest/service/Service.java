ba9263f613ab9cf37e3e3d7d53e5caa8fb0f8da2
Gerbrand van Dieijen <gerbrand@vandieijen.nl>
2011-07-19 19:58:29 +0200

    Using transactions, and commiting every 20 lines

--- src/main/java/com/xebia/rest/service/Service.java
+++ src/main/java/com/xebia/rest/service/Service.java
@@ -2,13 +2,20 @@ package com.xebia.rest.service;
 
 import java.io.*;
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceContext;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
+import javax.transaction.TransactionManager;
 
 import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,23 +26,35 @@ import com.xebia.rest.model.Record;
 
 @Controller
 public class Service {
+    private static final Logger log=LoggerFactory.getLogger(Service.class);
     
+    private static final int FLUSH_THRESHOLD=20;
     @PersistenceContext
     private EntityManager em;
+    
 
-	
     @PostConstruct
     public void buildDatabase() throws FileNotFoundException {
+        EntityTransaction tx = em.getTransaction();
+ 
         File dataFile = new File("/tmp/data.json");
         if (dataFile.exists()) {
+            tx.begin();
             BufferedReader br = new BufferedReader(new FileReader(dataFile));
             try {
+                int lines=0;
                 for (String line = br.readLine(); line != null; line = br.readLine()) {
                     ObjectMapper mapper = new ObjectMapper();
                     Record record = mapper.readValue(line, Record.class);
                     em.persist(record);
+                    lines++;
+                    if (lines%FLUSH_THRESHOLD==0) {
+                        tx.commit(); //in between commits
+                        log.debug("Wrote "+lines+" lines");
+                    }
                 }
-               
+            tx.commit();
+            
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
